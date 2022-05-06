4dd755273186fb64fb3d01d5103a062240488755
dnsgod <hoove@naver.com>
2022-05-06 13:27:14 +0900

    Print year when 3

--- hi.py
+++ hi.py
@@ -1,7 +1,10 @@
+for i in range(20):
+	if i % 3 == 0 and i % 5 != 0:
+		print("year")
+	elif i % 5 == 0 and i % 3 != 0:
+		print("dream")
+	if i % 3 == 0 and i % 5 == 0:
+		print("yeardream")
+	else:
+		print(i)
 
-irint('hello')
-print('hello')
-print('hello')
-print('hello')
-print('hello')
-print('hello')
