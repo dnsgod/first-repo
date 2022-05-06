cb6887a02a9765f6746daf56336cbbc73f7f2ff7
dnsgod <hoove@naver.com>
2022-05-06 13:34:01 +0900

    Yeardream

--- hi.py
+++ hi.py
@@ -1,9 +1,9 @@
-for i in range(20):
+for i in range(1,21):
 	if i % 3 == 0 and i % 5 != 0:
 		print("year")
 	elif i % 5 == 0 and i % 3 != 0:
 		print("dream")
-	if i % 3 == 0 and i % 5 == 0:
+	elif i % 3 == 0 and i % 5 == 0:
 		print("yeardream")
 	else:
 		print(i)
